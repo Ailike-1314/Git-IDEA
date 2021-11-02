97a64dec5537d468a4a95172a3aede6e8230460a
ailike <ailike@domain.com>
2021-11-02 15:39:11 +0800

    master修改

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -3,5 +3,6 @@ package org.example;
 public class test {
     public static void main(String[] args) {
         System.out.println("测试~~~test");
+        System.out.println("master。。。。。。修改");
     }
 }
