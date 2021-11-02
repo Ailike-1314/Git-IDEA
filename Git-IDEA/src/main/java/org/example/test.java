54496a8ec95efac2969c1584a8ea75556363e11d
ailikee <ailike@168.comm>
2021-11-02 19:57:17 +0800

    重新开始

---
+++ Git-IDEA/src/main/java/org/example/test.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试~~~test");
+        System.out.println("leaf1。。。。。。修改");
+    }
+}
