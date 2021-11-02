1d6b1443de45f50c85bbcebb97bc0a7633301aa5
ailike <ailike@domain.com>
2021-11-02 15:42:29 +0800

    leaf修改

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -3,6 +3,6 @@ package org.example;
 public class test {
     public static void main(String[] args) {
         System.out.println("测试~~~test");
-        System.out.println("master。。。。。。修改");
+        System.out.println("leaf1。。。。。。修改");
     }
 }
